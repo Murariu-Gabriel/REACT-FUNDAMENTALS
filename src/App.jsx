2e6d5119d046303a4cc4b30b8c83012bdef5584d
Gabriel <murariugabi23@gmail.com>
2023-05-17 08:22:56 +0300

    Learning basic component features and ES7 snippets for react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Component from "./components/myFirstComponent/MyFirstComponent"
+import ES7 from "./components/ES7-Snippets/ES7"
+
+const App = () => {
+  return (
+    <>
+      <Component />
+      <ES7 />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/myFirstComponent/MyFirstComponent.jsx
@@ -0,0 +1,14 @@
+const MyFirstComponent = () => {
+  return (
+    <div>
+      <h1>My First Component (not really)</h1>
+      <p>
+        - You have to remember that a component must start with a capital letter
+        unlike a function <br />
+        - It must return JSX meaning the HTML you are at this very moment writing <br />
+        - When invoked it you must must must close the tag meaning that your component needs to have this slash [ / ] at the end of the tag
+      </p>
+    </div>
+  )
+}
+export default MyFirstComponent
